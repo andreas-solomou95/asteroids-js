da03732253ce928ea2e1f479cc01e3bd1e2e4151
andreas-solomou <andrewsolomou@gmail.com>
2023-02-10 01:52:02 +0100

    ship styling changes & rotatable

--- index.js
+++ index.js
@@ -2,58 +2,99 @@ const FPS = 30;
 let renderedFrame = 0;
 
 const canvas = document.getElementById('gameCanvas');
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
+const counter = document.getElementById('frameCounter');
 
-const shipDiameter = 20;
-const shipState = {
-    x: window.innerWidth * 0.5 - shipDiameter,
-    y: window.innerHeight * 0.5 - shipDiameter
-};
+const toRads = (deg) => deg * (Math.PI / 180);
+
+const shipSpec = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2,
+    r: 10,
+    a: toRads(90)
+}
 
 setInterval(update, 1000 / FPS);
 
 function update() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
     space();
     ship();
-    context.restore();
-    context.translate(0, 0);
     thruster();
 
+    counter.innerHTML = renderedFrame % FPS + '/' + FPS;
     renderedFrame++;
 }
 
 function space(color = 'black') {
-    context.fillStyle = color;
-    context.fillRect(0, 0, window.innerWidth, window.innerHeight);
-    context.save();
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+    ctx.save();
 }
 
 function ship() {
-    context.beginPath();
-    context.moveTo(...translate(0, 0));
-    context.lineTo(...translate(2 * shipDiameter, shipDiameter));
-    context.lineTo(...translate(0, 2 * shipDiameter));
-    context.lineTo(...translate(0.7 * shipDiameter, shipDiameter));
-    context.lineTo(...translate(0, 0));
-    context.strokeStyle = "green";
-    context.lineWidth = 2;
-    context.stroke();
-    context.save();
+    const x = shipSpec.x;
+    const y = shipSpec.y;
+    const radius = shipSpec.r;
+    const angle = toRads(renderedFrame);
+
+    ctx.beginPath();
+    ctx.moveTo( // nose
+        x - radius * (Math.cos(angle) - Math.sin(angle)),
+        y + radius * (Math.sin(angle) + Math.cos(angle))
+    );
+    ctx.lineTo(
+        x + radius * Math.cos(angle),
+        y - radius * Math.sin(angle)
+    );
+    ctx.lineTo(
+        x - radius * (Math.cos(angle) + Math.sin(angle)),
+        y + radius * (Math.sin(angle) - Math.cos(angle))
+    );
+    ctx.lineTo(
+        x - 1/2 * radius * (Math.cos(angle)),
+        y + 1/2 * radius * (Math.sin(angle))
+    );
+    ctx.closePath();
+
+    ctx.shadowBlur = 5;
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+    ctx.shadowColor   = 'limegreen';
+    ctx.strokeStyle = 'limegreen';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    ctx.save();
 }
 
-function thruster(){
-    const keyframe = renderedFrame * 4 % 2.3;
-    context.fillStyle = "red";
-    context.beginPath();
-    context.moveTo(...translate(-0.2 * shipDiameter, 0.2 * shipDiameter));
-    context.lineTo(...translate(-0.8 * keyframe * shipDiameter, shipDiameter));
-    context.lineTo(...translate(-0.2 * shipDiameter, 1.8 * shipDiameter));
-    context.strokeStyle = "red";
-    context.lineWidth = 1;
-    context.stroke();
-}
+function thruster() {
+    const x = shipSpec.x;
+    const y = shipSpec.y;
+    const radius = shipSpec.r;
+    const angle = toRads(renderedFrame);
 
-const translate = (offsetX, offsetY) => [shipState.x + offsetX, shipState.y + offsetY];
+    const keyframe = renderedFrame % 2 + 1;
+    console.log(keyframe);
+
+    ctx.beginPath();
+    ctx.moveTo(
+        x - radius * (Math.cos(angle)),
+        y + radius * (Math.sin(angle))
+    );
+    ctx.lineTo(
+        x - keyframe * radius * (Math.cos(angle)),
+        y + keyframe * radius * (Math.sin(angle))
+    );
+
+    ctx.shadowBlur = 5;
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+    ctx.shadowColor   = 'red';
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+
+    ctx.stroke();
+    ctx.save();
+}
